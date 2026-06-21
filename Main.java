3ac15ea3ace517903ef92c9cb6dad32278f6ee7e
Syed Sian Wahadat <sianwahadat23@gmail.com>
2026-06-21 23:26:10 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import controller.StudentController;
+import model.StudentDAO;
+import view.StudentView;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StudentDAO dao =
+                new StudentDAO();
+
+        StudentView view =
+                new StudentView();
+
+        StudentController controller =
+                new StudentController(
+                        dao,
+                        view
+                );
+
+        view.setVisible(true);
+    }
+}
